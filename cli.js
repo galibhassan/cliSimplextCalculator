ad00fc25a0db2b7e90cf0c36e2f6daf5ee8b200f
Galib Hass <galib.hassan.0@gmail.com>
2018-03-13 11:31:04 +0100

    added help support

--- cli.js
+++ cli.js
@@ -2,38 +2,55 @@
 var stdin = process.openStdin();
 
 console.log(".--------------------------.");
-console.log("|      CLI Calculator      |    \t Arithmatic is simple.")
+console.log("|   CLI Calculator 1.0.0   |    \t\t Arithmatic was never so simple.")
 console.log("^--------------------------^");
 console.log("");
-console.log("For instruction, enter: \t instruct>> ");
-console.log("To quit, enter: \t\t Ctrl+C")
+console.log("For instruction, type: \t\t\t instruct..");
+console.log("For a specific functionality, type: \t help.. nameOfThatFunctionality");
+console.log("To quit, type: \t\t\t\t Ctrl+C");
 console.log("");
 var regExpForAddition = new RegExp("^add");
 var regExpForSubtraction = new RegExp("^subtract");
 var regExpForMultiplication = new RegExp("^multiply");
+var regExpForHelp = new RegExp("^help..");
 
 
 stdin.addListener("data", function (d) {
     var userInputString = d.toString().trim();
-    if (userInputString === 'instruct>>') {
+    if (userInputString === 'instruct..') {
         instruct();
     }
-    else if(regExpForAddition.test(userInputString)){
+    else if (regExpForAddition.test(userInputString)) {
         var entries = analyze(userInputString, 'add');
         var result = parseInt(entries[0]) + parseInt(entries[1]);
-        console.log("= "+ result + '\n');
+        console.log("= " + result + '\n');
     }
-    else if(regExpForSubtraction.test(userInputString)){
+    else if (regExpForSubtraction.test(userInputString)) {
         var entries = analyze(userInputString, 'subtract');
         var result = parseInt(entries[0]) - parseInt(entries[1]);
-        console.log("= " + result + '\n'); 
+        console.log("= " + result + '\n');
     }
-    else if(regExpForMultiplication.test(userInputString)){
+    else if (regExpForMultiplication.test(userInputString)) {
         var entries = analyze(userInputString, 'multiply');
         var result = parseInt(entries[0]) * parseInt(entries[1]);
-        console.log("= " + result + '\n'); 
+        console.log("= " + result + '\n');
+    }
+    else if (regExpForHelp.test(userInputString)) {
+        var helpKeyword = findHelpKeywordFrom(userInputString);
+        switch (helpKeyword) {
+            case 'add':
+                return console.log("add    just add two numbers a,b by add(a,b) \n");
+            case 'subtract':
+                return console.log("subtract   subtract two numbers a,b by subtract(a,b) \n");
+            case 'multiply':
+                return console.log("multiply    multiply two numbers a,b by multiply(a,b)");
+            default:
+                return console.log("Are you from Mars? Make sense of what you type! \n");
+        }
+
     }
     else (console.log("ERROR:\t Type atleast something sensible, will you?!\n"));
+
 
     function instruct() {
         console.log('.------------------------------------------.----------------------.');
@@ -42,18 +59,18 @@ stdin.addListener("data", function (d) {
         console.log('| To add two numbers, a and b              |  add(a,b)            |');
         console.log('| To subtract two numbers, a from b        |  subtract(a,b)       |');
         console.log('| To multiply two numbers, a and b         |  multiply(a,b)       |');
-        console.log('| To divide two numbers, a by b            |  multiply(a,b)       |');
         console.log('^------------------------------------------^----------------------^');
         console.log("");
     }
 
 
 
-function analyze(theString, operation){
-    return theString.split(operation)[1].split('(')[1].split(')')[0].split(',')
-}
 
+    function analyze(theString, operation) {
+        return theString.split(operation)[1].split('(')[1].split(')')[0].split(',')
+    }
 
-
-
+    function findHelpKeywordFrom(theString) {
+        return theString.split("help.. ")[1];
+    }
 });
